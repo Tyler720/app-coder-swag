84c27010cb07908e73b4ac6aa6ac20d3ce691d68
Tyler Cantlon <Tyler@Tyler-Cantlons-MacBook.local>
2019-04-25 18:20:12 -0500

    third commit

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Tyler Cantlon on 4/25/19.
+//  Copyright © 2019 Tyler. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+}
